eacaeda953058a33ea6145ea7fa57e7eee69a2fb
Carl de Guia <carl.deguia@phitopolis.com>
2020-08-09 23:07:49 +0800

    FINAL INITIAL CODE

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.scss';
+import Router from './Router/Router';
+import { FirebaseProvider } from './firebase/useFirebase';
+import ROOT_ROUTES from './pages/rootRoutes';
+
+const App: React.FC = () => (
+  <FirebaseProvider>
+    <Router rootRoutes={ROOT_ROUTES} />
+  </FirebaseProvider>
+);
 
 export default App;
---
+++ src/pages/rootRoutes.ts
@@ -0,0 +1,19 @@
+import { RouteObject } from '../Router/model';
+
+const ROOT_ROUTES: RouteObject[] = [
+  // {
+  //   path: '/',
+  //   name: 'Landing Page',
+  //   exact: true,
+  //   hideOnAuth: true,
+  //   component: Landing,
+  //   isPublic: true,
+  // },
+  // {
+  //   path: '/home',
+  //   name: 'Home Page',
+  //   component: Home,
+  // },
+];
+
+export default ROOT_ROUTES;
